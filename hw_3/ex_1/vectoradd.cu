b6c57710811de9b2347be4609e3f7b291c809e8a
wertysas <johanerics@gmail.com>
2022-12-13 17:59:56 +0100

    [III vectoradd] added timing functionality

--- hw_3/ex_1/vectoradd.cu
+++ hw_3/ex_1/vectoradd.cu
@@ -17,24 +17,29 @@ __global__ void vecAdd(DataType *in1, DataType *in2, DataType *out, uint len) {
   }
 }
 
-//@@ Insert code to implement timer start
-
-//@@ Insert code to implement timer stop
+// Timer function
+double time() {
+  struct timeval tp;
+  gettimeofday(&tp,NULL);
+  return ((double)tp.tv_sec + (double)tp.tv_usec*1.e-6);
+}
 
 
 int main(int argc, char **argv) {
-  
   uint inputLength;
   DataType *hostInput1;
   DataType *hostInput2;
   DataType *hostOutput;
   DataType *resultRef;
-  DataType *deviceInput1;
-  DataType *deviceInput2;
+  DataType *deviceInput1; DataType *deviceInput2;
   DataType *deviceOutput;
 
   // Input length reading
-  inputLength = int(*argv[1]);
+  if (argc != 2) {
+    printf("Error wrong format! Correct Usage :\t ./vectoradd [array length]");
+    return -1;
+  }
+  inputLength = atoi(argv[1]);
   printf("The input length is %d\n", inputLength);
   
   // Host memory allocation
@@ -51,10 +56,11 @@ int main(int argc, char **argv) {
   for (uint i=0; i<inputLength; i++) { hostInput2[i] = d(e); }
   
   // Computation of reference result on host
+  double t0 = time();
   for (uint i=0; i<inputLength; i++) {
     resultRef[i] =  hostInput1[i] + hostInput2[i];
   }
-    
+  double cpuTiming = time() - t0;
 
   // GPU memory allocation
   cudaMalloc(&deviceInput1, vsizeB);
@@ -62,6 +68,7 @@ int main(int argc, char **argv) {
   cudaMalloc(&deviceOutput, vsizeB);
 
   // Copy memory to the GPU
+  double tb = time();
   cudaMemcpy(&deviceInput1, hostInput1, vsizeB, cudaMemcpyHostToDevice);
   cudaMemcpy(&deviceInput2, hostInput2, vsizeB, cudaMemcpyHostToDevice);
 
@@ -70,10 +77,14 @@ int main(int argc, char **argv) {
   uint gridSize  = (inputLength+blockSize-1) / blockSize;
 
   // Launch the GPU Kernel
+  t0 = time();
   vecAdd<<<gridSize, blockSize>>>(deviceInput1, deviceInput2, deviceOutput, inputLength);
+  cudaDeviceSynchronize();
+  double gpuTiming = time() - t0;
 
   // Copy the GPU memory back to the CPU
   cudaMemcpy(&hostOutput, &deviceOutput, vsizeB, cudaMemcpyDeviceToHost);
+  double totalTiming = time() - tb;
 
   // Compare the output with the reference
   DataType max_diff = 1e-7;
@@ -82,8 +93,8 @@ int main(int argc, char **argv) {
       printf("Error results differ more than maximum value (>%f)\n", max_diff);
       printf("Host Calculated Value: %f\n", resultRef[i]);
       printf("Device Calculated Value: %f\n", hostOutput[i]);
+    }
   }
-
   //@@ Free the GPU memory here
   cudaFree(deviceInput1);
   cudaFree(deviceInput2);
@@ -94,6 +105,8 @@ int main(int argc, char **argv) {
   free(hostInput2);
   free(hostOutput);
   free(resultRef);
-
+  printf("CPU vector addition time: %f", cpuTiming);
+  printf("GPU vector addition time: %f", gpuTiming);
+  printf("GPU vector addition + data transfer time: %f", totalTiming);
   return 0;
 }
